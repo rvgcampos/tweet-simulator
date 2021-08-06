400c24eef5fc2bba5997b88f379dfe27b667f083
Renato <renato.vini@outlook.com>
2021-08-06 16:17:50 -0300

    Adicionado link

---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <span class="navbar-brand">Simulador de Tweets</span>
+      <button
+        class="btn pl-4 pr-4"
+        @click="openCloseForm"
+        :class="{ 'btn-success': !showForm, 'btn-danger': showForm }"
+      >
+        {{ !showForm ? "Nuevo Tweet" : "Cerrar Tweet" }}
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    showForm: Boolean,
+    openCloseForm: Function,
+  },
+};
+</script>
+
+<style></style>
